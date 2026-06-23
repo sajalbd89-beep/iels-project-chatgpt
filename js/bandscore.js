a828bc93be7c795a0e56d76bde5642288ba130f8
sajalbd89-beep <sajal.bd89@gmail.com>
2026-06-23 22:48:32 +0600

    Update bandscore.js

--- js/bandscore.js
+++ js/bandscore.js
@@ -1,1 +1,21 @@
+function getBand(score){
 
+if(score>=39) return "Band 9";
+
+if(score>=37) return "Band 8.5";
+
+if(score>=35) return "Band 8";
+
+if(score>=33) return "Band 7.5";
+
+if(score>=30) return "Band 7";
+
+if(score>=27) return "Band 6.5";
+
+if(score>=23) return "Band 6";
+
+if(score>=19) return "Band 5.5";
+
+return "Band 5";
+
+}
