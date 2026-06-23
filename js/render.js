5ec8f5c11737a12389aa22d8afbb7ae5d1bfe687
sajalbd89-beep <sajal.bd89@gmail.com>
2026-06-23 22:48:00 +0600

    Update render.js

--- js/render.js
+++ js/render.js
@@ -1,1 +1,66 @@
+function renderQuestions(){
 
+const container =
+document.getElementById(
+"questionContainer"
+);
+
+container.innerHTML="";
+
+
+exam.questions.forEach(
+(q,index)=>{
+
+const div =
+document.createElement(
+"div"
+);
+
+div.className =
+"question";
+
+
+if(q.type==="Multiple Choice"){
+
+div.innerHTML=`
+
+<p>
+
+${index+1}. ${q.question}
+
+</p>
+
+<input
+id="q${index}"
+placeholder="Answer A/B/C/D">
+
+`;
+
+}
+
+else{
+
+div.innerHTML=`
+
+<p>
+
+${index+1}. ${q.question}
+
+</p>
+
+<input
+id="q${index}"
+placeholder="Type answer">
+
+`;
+
+}
+
+
+container.appendChild(
+div
+);
+
+});
+
+}
