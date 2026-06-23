744c447e5d415a6b354289797e1c7739bb11b366
sajalbd89-beep <sajal.bd89@gmail.com>
2026-06-23 22:47:39 +0600

    Update payload.js

--- js/payload.js
+++ js/payload.js
@@ -1,1 +1,39 @@
+function getExamData(){
 
+const params =
+new URLSearchParams(
+window.location.search
+);
+
+const payload =
+params.get("payload");
+
+if(!payload){
+
+alert(
+"No exam found."
+);
+
+throw new Error();
+
+}
+
+try{
+
+return JSON.parse(
+atob(payload)
+);
+
+}
+
+catch{
+
+alert(
+"Broken exam link."
+);
+
+throw new Error();
+
+}
+
+}
