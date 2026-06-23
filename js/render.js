0e69b2f7bea133c6e358892a9065ec37ba1e6d6d
sajalbd89-beep <sajal.bd89@gmail.com>
2026-06-23 23:25:19 +0600

    Update render.js

--- js/render.js
+++ js/render.js
@@ -5,19 +5,62 @@ document.getElementById(
 "questionContainer"
 );
 
+const palette =
+document.getElementById(
+"questionPalette"
+);
+
 container.innerHTML="";
+palette.innerHTML="";
 
 
 exam.questions.forEach(
 (q,index)=>{
+
+// Palette Button
+
+const box =
+document.createElement(
+"button"
+);
+
+box.className=
+"palette-btn";
+
+box.innerText=
+index+1;
+
+box.onclick=()=>{
+
+document
+.getElementById(
+`question${index}`
+)
+.scrollIntoView({
+
+behavior:"smooth"
+
+});
+
+};
+
+palette.appendChild(
+box
+);
+
+
+// Question Card
 
 const div =
 document.createElement(
 "div"
 );
 
-div.className =
+div.className=
 "question";
+
+div.id=
+`question${index}`;
 
 
 if(q.type==="Multiple Choice"){
@@ -58,8 +101,7 @@ placeholder="Type answer">
 
 
 container.appendChild(
-div
-);
+div);
 
 });
 
