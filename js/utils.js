ef0bdba68a412030a3182060d3a752c0b77154d9
sajalbd89-beep <sajal.bd89@gmail.com>
2026-06-23 23:39:36 +0600

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,1 +1,27 @@
+function normalizeAnswer(answer) {
 
+return answer
+.trim()
+.toLowerCase()
+.replace(/\s+/g," ");
+
+}
+
+
+function saveAnswer(index,value){
+
+localStorage.setItem(
+`answer_${index}`,
+value
+);
+
+}
+
+
+function getSavedAnswer(index){
+
+return localStorage.getItem(
+`answer_${index}`
+) || "";
+
+}
