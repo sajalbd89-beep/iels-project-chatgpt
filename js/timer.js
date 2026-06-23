70ec6327edc44a2de339c35b3170f327e3998b6a
sajalbd89-beep <sajal.bd89@gmail.com>
2026-06-23 23:40:27 +0600

    Update timer.js

--- js/timer.js
+++ js/timer.js
@@ -1,1 +1,28 @@
+let seconds=3600;
 
+function startTimer(){
+
+setInterval(()=>{
+
+seconds--;
+
+let m=Math.floor(seconds/60);
+
+let s=seconds%60;
+
+document
+.getElementById(
+"timer"
+)
+.innerText=
+`${m}:${String(s).padStart(2,"0")}`;
+
+if(seconds<=0){
+
+submitExam();
+
+}
+
+},1000);
+
+}
