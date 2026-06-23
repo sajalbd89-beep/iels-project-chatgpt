c213764e7069272a2bf1b1d7cdcd94eea26e001b
sajalbd89-beep <sajal.bd89@gmail.com>
2026-06-23 22:48:49 +0600

    Update scorer.js

--- js/scorer.js
+++ js/scorer.js
@@ -1,1 +1,58 @@
+function submitExam(){
 
+let score=0;
+
+
+exam.questions.forEach(
+(q,index)=>{
+
+const answer =
+document
+.getElementById(
+`q${index}`
+)
+.value
+.trim()
+.toLowerCase();
+
+
+if(
+answer===
+q.answer
+.toLowerCase()
+){
+
+score++;
+
+}
+
+});
+
+
+document
+.getElementById(
+"scoreText"
+)
+innerText=
+`Correct Answers:
+
+${score}/${exam.questions.length}`;
+
+
+document
+.getElementById(
+"bandText"
+)
+.innerText=
+getBand(score);
+
+
+document
+.getElementById(
+"resultModal"
+)
+.classList.remove(
+"hidden"
+);
+
+}
