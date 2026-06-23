73834dc6fbfbc5808b7098f80f9a23d610865311
sajalbd89-beep <sajal.bd89@gmail.com>
2026-06-23 23:40:02 +0600

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,1 +1,20 @@
+function restoreAnswers(){
 
+exam.questions.forEach(
+(q,index)=>{
+
+const input =
+document.getElementById(
+`q${index}`
+);
+
+if(input){
+
+input.value =
+getSavedAnswer(index);
+
+}
+
+});
+
+}
