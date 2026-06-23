90a2bd33fc305e247ce6afd9f96c26520f43e031
sajalbd89-beep <sajal.bd89@gmail.com>
2026-06-23 22:47:01 +0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,33 @@
+const exam = getExamData();
 
+document
+.getElementById("passageTitle")
+.innerText = exam.title;
+
+document
+.getElementById("passageContent")
+.innerText = exam.passage;
+
+renderQuestions();
+
+startTimer();
+
+document
+.getElementById("submitBtn")
+.addEventListener(
+"click",
+submitExam
+);
+
+
+function closeModal(){
+
+document
+.getElementById(
+"resultModal"
+)
+.classList.add(
+"hidden"
+);
+
+}
